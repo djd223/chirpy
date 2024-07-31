1a900203e677e0805c57f348489d5935442ed267
djd223 <davissond99@gmail.com>
2024-07-30 17:25:49 -0700

    Custom handlers and routing

--- main.go
+++ main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
 )
+
+type apiConfig struct {
+	fileserverHits int
+}
 
 func main() {
 	const filePathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
+	apiCfg := apiConfig{}
+	mux.Handle("/app/*", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot)))))
+	mux.HandleFunc("GET /healthz", handlerReadiness)
+	mux.HandleFunc("GET /metrics", apiCfg.handlerCounter)
+	mux.HandleFunc("/reset", apiCfg.resetCounter)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -19,4 +28,28 @@ func main() {
 
 	log.Printf("Serving %s on port: %s\n", filePathRoot, port)
 	log.Fatal(srv.ListenAndServe())
+
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) handlerCounter(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hits: " + strconv.Itoa(cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) resetCounter(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Counter reset successfully"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
 }
